7054bd04f2e90fa8db75311d3ff83bc5a58c988b
Josef Zacek <info@josefzacek.com>
2015-02-02 21:12:13 +0000

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+http://www.brucelawson.co.uk/2015/why-we-cant-do-real-responsive-images-with-css-or-javascript/
+
+Change image using CSS depending on device width
+            
+
+
+
+
---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Josef Zacek - website starter pack</title>
+  <meta name="author" content="www.josefzacek.com">
+  <meta name="description" content="">
+  <meta name="keywords" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="shortcut icon" href="images/favicon.ico" />
+  <link rel="stylesheet" href="css/reset.css">
+  <link rel="stylesheet" href="css/style.css">
+<!--[if lt IE 9]>
+	<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+</head>
+
+<body>
+<!--[if lt IE 8]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+<![endif]-->
+
+<div id="wrapper">
+    
+    <nav>
+        <ul id="nav">
+            <li>
+                <a href="#">Home</a>
+                <a href="#">About</a>
+                <a href="#">Products</a>
+                <a href="#">Gallery</a>
+                <a href="#">Contact</a>
+            </li>
+        </ul>
+        
+    </nav>
+    
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <img id="thingy" src="images/mobile.jpg" alt="a mankini">
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+
+
+</div>
+<!-- wrapper end -->
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+<script src="js/tinynav.min.js"></script>
+<script src="js/scripts.js"></script>
+<script src="js/css3-mediaqueries.js"></script>
+</body>
+</html>
+
